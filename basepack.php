ccb73d1ad2857272681f2b351bd5f074fd792185
Piotr Łepkowski <ozrabal@gmail.com>
2015-04-08 19:34:29 +0200

    Initial commit

---
+++ basepack.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: basepack
+ * Plugin URI: http://
+ * Description: A brief description of the plugin.
+ * Version: 1.0.0
+ * Author: 
+ * Author URI: http://webkowski.com
+ * Text Domain: bpck
+ * Domain Path: Optional. Plugin's relative directory path to .mo files. Example: /locale/
+ * Network: Optional. Whether the plugin can only be activated network wide. Example: true
+ * License: A short license name. Example: GPL2
+ */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
