e9748a1c96c945dae30d419705b2a71a70a3e226
Piotr Łepkowski <ozrabal@gmail.com>
2015-04-08 22:24:01 +0200

    autoloader
    autoloader na baize
    https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-auto
    loader-examples.md

---
+++ modules/cookie/cookie.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Modules\Cookie;
+
+class Cookie {
+    
+    public function __construct() {
+        echo __CLASS__;
+    }
+    
+}
