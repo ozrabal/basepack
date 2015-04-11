dab04b1137637d49bbf5e9e07270cd7798d5d021
Piotr Łepkowski <ozrabal@gmail.com>
2015-04-11 23:12:39 +0200

    debug mode

--- core/_inc/functions.php
+++ core/_inc/functions.php
@@ -13,3 +13,24 @@ function dump( $var, $echo = 1 ) {
         echo $output;
     }
 }
+
+//define('WP_DEBUG_LOG', true);
+//@todo linia i nazwa pliku
+function dlog($message) {
+    //if (WP_DEBUG === true) {
+        if (is_array($message) || is_object($message)) {
+            error_log(print_r($message, true));
+        } else {
+            error_log($message);
+        }
+    //}
+}
+
+
+function dbug($message){
+    if (WP_DEBUG === true) {
+        
+        dump($message);
+    }
+    dlog($message);
+}
