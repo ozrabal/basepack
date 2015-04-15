dc12aff6ae67b6c4b69a38c16d74fc188628464e
Piotr Łepkowski <ozrabal@gmail.com>
2015-04-15 23:02:37 +0200

    test setup taxonomy custom fields

--- modules/test/test.php
+++ modules/test/test.php
@@ -137,6 +137,133 @@ class Test {
 	);
 	
 	new \Basepack\Core\Metabox( $test_meta );
+        
+        //add taxonomy
+        $labels = array(
+		'name'              => _x( 'Test taxonomy', 'taxonomy general name' ),
+		'singular_name'     => _x( 'Test taxonomy', 'taxonomy singular name' ),
+		'search_items'      => __( 'Search terms' ),
+		'all_items'         => __( 'All terms' ),
+		'parent_item'       => __( 'Parent Term' ),
+		'parent_item_colon' => __( 'Parent Term:' ),
+		'edit_item'         => __( 'Edit Term' ),
+		'update_item'       => __( 'Update Term' ),
+		'add_new_item'      => __( 'Add New Term' ),
+		'new_item_name'     => __( 'New Term Name' ),
+		'menu_name'         => __( 'Term' ),
+	);
+
+	$args = array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'testtaxonomy' ),
+	);
+
+	register_taxonomy( 'test_taxonomy', array( 'test' ), $args );
+        
+        $test_tax_meta = array(
+	    'name'      => 'test_tax_meta',
+	    'title'     => 'Pola dodatkowe w taxonomii',
+	    'tax'       => 'test_taxonomy',
+	    'elements'  => array(
+		array(
+		    'type'	=> 'text',
+		    'name'	=> 'text',
+		    'params'	=> array(
+			'label'	    => 'Text Label',
+			'comment'   => 'Comment',
+			'class'	    => 'large-text'
+		    ),
+		),
+                array(
+		    'type'	=> 'textarea',
+		    'name'	=> 'textarea',
+		    'params'	=> array(
+			'label'	    => 'Textarea Label',
+			'comment'   => 'Comment',
+			'class'	    => 'large-text'
+		    ),
+		),
+		array(
+		    'type'	=> 'wysiwyg',
+		    'name'	=> 'wysiwyg',
+		    'params'	=> array(
+			'label'	    => 'Wysiwyg Label',
+			'comment'   => 'Comment',
+			'class'	    => '',
+			'options'   => array(
+			    'tinymce'	=> true
+			)
+		    ),
+		),
+		array(
+		    'type'	=> 'checkbox',
+		    'name'	=> 'checkbox',
+		    'params'	=> array(
+			'label'	    => 'Checkbox Label',
+			'comment'   => 'Comment',
+			'class'	    => ''
+		    ),
+		),
+		array(
+		    'type'	=> 'image',
+		    'name'	=> 'image',
+		    'params'	=> array(
+			'label'	    => 'Image Label',
+			'comment'   => 'Comment',
+			'class'	    => ''
+		    ),
+		),
+		array(
+		    'type'	=> 'attachment',
+		    'name'	=> 'attachment',
+		    'params'	=> array(
+			'label'	    => 'Attachment Label',
+			'comment'   => 'Comment',
+			'class'	    => ''
+		    ),
+		),
+		array(
+		    'type'	=> 'select',
+		    'name'	=> 'select',
+		    'params'	=> array(
+			'label'	=> 'Select Label',
+			'comment'   => 'Comment',
+			'class'	    => '',
+			'options'   => array(
+			    'key1'  => 'value1',
+			    'key2'  => 'value2'
+			)
+		    ),
+		),
+		array(
+		    'type'	=> 'repeatable',
+		    'name'	=> 'repeatable',
+		    'params'	=> array(
+			'label'	=> 'Repeatable Label',
+			'comment'   => 'Comment',
+			'class'	    => '',
+			'repeater'  => array(
+			    array(
+				'type'	=> 'text',
+				'name'	=> 'repeatable_text',
+				'params'=> array(
+				    'label'	=> 'Repeatable Text Label',
+				    'comment'   => 'Comment',
+				    'class'	=> 'large-text'
+				)
+			    )
+			)
+		    )
+		)
+            )
+        );
+        
+        new \Basepack\Core\Taxmeta( $test_tax_meta );
+        
     }
 
 }
