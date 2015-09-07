28fa858cc709e49beed09d66bc8c64838c05f485
Piotr Łepkowski <ozrabal@gmail.com>
2015-09-07 16:01:01 +0200

    default color value

--- core/form/color.php
+++ core/form/color.php
@@ -10,6 +10,7 @@ namespace Basepack\Core\Form;
 
 class Color extends Formelement {
     protected $type = 'color';
+    protected $default_color = '#ffffff';
 
     /**
      *
@@ -19,6 +20,7 @@ class Color extends Formelement {
     public function __construct( $form, $name ) {
 	parent::__construct( $form, $name );
 	$this->set_class( 'color-field' );
+	
     }
 
     /**
@@ -29,7 +31,12 @@ class Color extends Formelement {
     public function set_palettes( $palettes ) {
 	$this->palettes = $palettes;
     }
-
+    public function set_default_color( $default_color ){
+	$this->default_color = $default_color;
+    }
+    public function get_default_color(){
+	return $this->default_color;
+    }
     /**
      * dolacza skrypty js
      */
@@ -38,7 +45,9 @@ class Color extends Formelement {
 	wp_enqueue_script( 'field-color',  BASEPACK_PLUGIN_URL . 'core/form/field-color.js', array( 'jquery', 'wp-color-picker' ), BASEPACK_VERSION );
 	if(!empty($this->palettes)){
             wp_localize_script( 'wp-color-picker', 'palettes', json_encode( $this->palettes ) );
+	    
         }
+	wp_localize_script( 'wp-color-picker', 'default_color', '#ffffff' );
     }
 
     /**
@@ -51,6 +60,9 @@ class Color extends Formelement {
         parent::render();
         if( isset( $this->callback ) ) {
 	    $this->do_callback( $this->callback );
+	}
+	if($this->get_value() == '' ){
+	    $this->set_value($this->get_default_color());
 	}
         return  $this->get_before() . $this->get_label() . '<input ' . $this->get_disabled() . $this->id() . $this->type() . $this->name() . $this->value() . $this->cssclass() . '/>' . $this->get_message() . $this->get_comment( '<p class="description">%s</p>' ) . $this->get_after();
     }
--- core/form/field-color.js
+++ core/form/field-color.js
@@ -1,12 +1,19 @@
 jQuery(function(jQuery) {
-    if( typeof palettes !== undefined ){
+    if( typeof default_color !== 'undefined' ){
+	var predefined_color = default_color;
+    } else {
+	var predefined_color = '#ffffff';
+    }
+    if( typeof palettes !== 'undefined' ){
         var predefined_palettes = jQuery.parseJSON( palettes );
     } else {
         var predefined_palettes = {};
     }
     function init(){
 	jQuery( '.color-field' ).wpColorPicker({
+	    color: '#ffffff',
 	    palettes: predefined_palettes
+	    
 	});
     }
     init();
