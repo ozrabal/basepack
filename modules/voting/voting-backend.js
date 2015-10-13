00b0cb60d535c477e41bd6849e49d93932b527f0
Piotr Łepkowski <ozrabal@gmail.com>
2015-10-13 13:42:58 +0200

    backend chart refactor

--- modules/voting/voting-backend.js
+++ modules/voting/voting-backend.js
@@ -1,56 +1,74 @@
-!function ($) {
-  var
-      chart,
-      width = 800,
-      bar_height = 50,
-      height = bar_height * chart_data.names.length;
-    
-var left_width = 10;
-  /* step 1 */
-  chart = d3.select($("#voting-results")[0])
-    .append('svg')
-    .attr('class', 'chart')
-    .attr('width', width)
-    .attr('height', height);
+var chart = document.getElementById("chart"),
+    axisMargin = 15,
+    margin = 10,
+    valueMargin = 5,
+    width = chart.offsetWidth,
+    height = chart.offsetHeight,
+    barHeight = (height-axisMargin-margin*2)* 0.8/data.length,
+    barPadding = (height-axisMargin-margin*2)*0.2/data.length,
+    data, bar, svg, scale, xAxis, labelWidth = 0;
 
-var x, y;
-x = d3.scale.linear()
-   .domain([0, d3.max(chart_data.scores)])
-   .range([0, width]);
+max = d3.max(data.map(function(i){
+  return i[1];
+}));
 
-y = d3.scale.ordinal()
-   .domain(chart_data.scores)
-   .rangeBands([0, height]);
+svg = d3.select(chart)
+  .append("svg")
+  .attr("width", width)
+  .attr("height", 250);
 
-chart.selectAll("rect")
-   .data(chart_data.scores)
-   .enter().append("rect")
-   .attr("x", 0)
-   .attr("y", y)
-   .attr("width", x)
-   .attr("height", y.rangeBand());
+bar = svg.selectAll("g")
+  .data(data)
+  .enter()
+  .append("g");
 
-chart.selectAll("text.score")
-  .data(chart_data.scores)
-  .enter().append("text")
-  .attr("x", function(d) { return x(d) ; })
-  .attr("y", function(d){ return y(d) + y.rangeBand()/2; } )
-  .attr("dx", -5)
-  .attr("dy", ".36em")
+bar.attr("class", "bar")
+  .attr("cx",0)
+  .attr("transform", function(d, i) {
+     return "translate(" + margin + "," + (i * (barHeight + barPadding) + barPadding) + ")";
+  });
+
+bar.append("text")
+  .attr("class", "label")
+  .attr("y", barHeight / 2)
+  .attr("dy", ".35em") //vertical align middle
+  .text(function(d){
+    return d[0];
+  }).each(function() {
+    labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
+  });
+
+scale = d3.scale.linear()
+  .domain([0, max])
+  .range([0, width - margin*2 - labelWidth]);
+
+xAxis = d3.svg.axis()
+  .scale(scale)
+  .tickSize(-height + 2*margin + axisMargin)
+  .orient("bottom");
+
+bar.append("rect")
+  .attr("transform", "translate("+labelWidth+", 0)")
+  .attr("height", barHeight)
+  .attr("width", function(d){
+    return scale(d[1]);
+  });
+
+bar.append("text")
+  .attr("class", "value")
+  .attr("y", barHeight / 2)
+  .attr("dx", -valueMargin + labelWidth) //margin right
+  .attr("dy", ".35em") //vertical align middle
   .attr("text-anchor", "end")
-  .attr('class', 'score')
-  .text(String);
+  .text(function(d){
+    return d[1];
+  })
+ .attr("x", function(d){
+    var width = this.getBBox().width;
+    return Math.max(width + valueMargin, scale(d[1]));
+  });
 
-chart.selectAll("text.name")
-  .data(chart_data.names)
-  .enter().append("text")
-  .attr("x", left_width / 2)
-  .attr("y", function(d, i){ return y(chart_data.scores[i]) + y.rangeBand()/2; } )
-  .attr("dy", ".36em")
-  .attr("text-anchor", "start")
-  .attr('class', 'name')
-  .text(String);
-
-
-}(window.jQuery);
-
+svg.insert("g",":first-child")
+ .attr("class", "axis")
+ .attr("transform", "translate(" + (margin + labelWidth) + ","+ (height - axisMargin - margin)+")")
+ .call(xAxis);
