67a5645ea8a2ce46011be0500704fabd07f1db19
ozrabal <ozrabal@gmail.com>
2015-10-27 22:20:21 +0100

    create columns in content initial commit

---
+++ modules/columns/columns.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Columns;
+
+
+class Columns {
+    
+    /**
+     * 
+     * @param \Basepack\Core\Autoloader $loader
+     */
+    public function __construct( \Basepack\Core\Autoloader $loader ) {
+        
+    }
+    
+    
+}
